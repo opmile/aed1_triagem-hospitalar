d4af60f08cca9089950445207e571580c8352fc8
giovanafporto07-lab <giovanafporto07@gmail.com>
2026-02-11 21:04:35 -0300

    Fix header guard in prontuario_menu.h

--- prontuario/prontuario_menu.h
+++ prontuario/prontuario_menu.h
@@ -3,7 +3,6 @@
 
 #include "prontuario.h"
 
-/* Executa o menu do prontuário. Retorna ao chamador quando o usuário escolher voltar. */
 void menu_prontuario(Prontuario* prontuario);
 
-#endif /* PRONTUARIO_MENU_H */
+#endif 
